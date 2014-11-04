f0c48984d65197ddc9ccd83a15e931d30d8c1e78
f95johansson <f95johansson@gmail.com>
2014-11-04 14:17:04 +0100

    Game folder structure

---
+++ Parrot/main.lua
@@ -0,0 +1,24 @@
+local class     = require 'lib.middleclass'
+local bump      = require 'lib.bump'
+local gamera    = require 'lib.gamera'
+
+
+function love.load()
+
+
+end
+
+function love.update( dt )
+
+
+end
+
+function love.draw()
+
+
+end
+
+function love.keypressed(key, isrepeat)
+
+end
+
